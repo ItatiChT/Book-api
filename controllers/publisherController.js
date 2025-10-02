3580647409d66778732d027c032e0d419d06fb1c
camiisd <liccamilasoto@gmail.com>
2025-10-02 20:49:10 -0300

    Se modifican los archivos correspondientes a publisherModel.js publisherController.js

---
+++ controllers/publisherController.js
@@ -0,0 +1,32 @@
+//Importamos el modelo y la vista
+import { publisherModel } from '../models/publisherModel'
+import { responseView } from '../views/publisherView'
+
+const publisherController = {
+    //Obtener todos las editoriales
+    getPublishers: () => {
+        const publishers = publisherModel.readPublishers(); 
+        return responseView.formatResponse(publishers);
+    },
+
+    //Método para agregar editorial
+    addPublisher: (newPublisher) => {
+        const publishers = publisherModel.readPublishers();  
+        publishers.push(newPublisher);
+        publisherModel.writePublishers(publishers); 
+        return responseView.formatResponse({ message: 'Editorial agregada con éxito.' });
+    },
+
+    //Eliminar editorial por ID
+    deletePublisher: (id) => {
+        let publishers = publisherModel.readPublishers();
+        const newPublishers = publishers.filter(p => p.id !== id);
+        if (newPublishers.length === publishers.length) {
+            return responseView.formatResponse({ error: 'Editorial no encontrada.' });
+        }
+        publisherModel.writePublishers(newPublishers);
+        return responseView.formatResponse({ message: 'Editorial eliminada con éxito.' });
+    }
+}
+
+export { publisherController }
---
+++ models/publisherModel.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, '../data/publishers.json');
+
+const publisherModel = {
+  //Leemos el archivo JSON y obtenemos las editoriales
+  readPublishers: () => {
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data)
+  },
+  
+  //Escribir el archivo JSON con nueva data
+  writePublishers: (publishers) => {
+    const jsonData = JSON.stringify(publishers, null, 2);
+    fs.writeFileSync(filePath, jsonData);
+  }
+};
+
+export { publisherModel };
